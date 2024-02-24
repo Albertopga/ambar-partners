97c543d46888f0ccc81e29f4b377a2bd8aa9f419
Alberto <albertopga@gmail.com>
2024-02-24 16:34:50 +0100

    WIP challenge 2

--- src/views/UserDetailView.vue
+++ src/views/UserDetailView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MainLayout from '@/layouts/MainLayout.vue'
+import { reactive } from 'vue'
 /*
 Reto 2: Vista de Datos del Usuario.
 
@@ -70,12 +71,81 @@ const userData = {
   ein: '20-9487066',
   ssn: '661-64-2976',
 }
+
+/** Suponiendo que userData me llegue de un servicio, lo primero que haria seria crear objetos organizados y reactivos, 
+ * con los datos agrupados que quiero mostrar y editar en las diferentes secciones */
+const personalDataFields = [
+  "firstName",
+  "lastName",
+  "maidenName",
+  "age",
+  "gender",
+  "birthDate",
+  "image",
+  "height",
+  "weight",
+  "eyeColor",
+  "hair",
+  "bloodGroup",
+  "height",
+  "weight"
+]
+
+const contactDataFields = ["email", "phone", "address"]
+const employmentDataFields = ["ein", "ssn", "company", "university"]
+const credentialsDataFields = ["id", "username", "password", "domain", "ip", "macAddress"]
+
+const prepareObject = (fields) => {
+  const res = {}
+  fields.forEach(key => res[key] = userData[key]);
+  return res
+}
+
+const personalData = reactive(prepareObject(personalDataFields))
+const contactData = reactive(prepareObject(contactDataFields))
+const employmentData = reactive(prepareObject(employmentDataFields))
+const credentialsData = reactive(prepareObject(credentialsDataFields))
+const bankData = reactive(userData.bank)
+
+console.log(personalData)
 </script>
 
 <template>
   <MainLayout>
-    <pre>
-    {{ userData }}
-    </pre>
+    <header class="bg-gray-800 py-4 text-center text-secondary">
+      <h1 class="text-2xl font-semibold">Datos del usuario</h1>
+    </header>
+    <main class="container mx-auto py-8">
+      <section class="bg-white shadow-md rounded-lg p-6 mb-8">
+        <h2 class="text-xl font-semibold mb-4">Datos personales</h2>
+        <ul>
+          <li v-for="value, key, a in personalData"><strong>{{ key }}:</strong> {{ value }}</li>
+        </ul>
+      </section>
+      <section class="bg-white shadow-md rounded-lg p-6 mb-8">
+        <h2 class="text-xl font-semibold mb-4">Datos bancarios</h2>
+        <ul>
+          <li><strong>Banco:</strong> {{ 'hola' }}</li>
+          <li><strong>Número de cuenta:</strong> {{ 'hola' }}</li>
+          <!-- Agrega más detalles según los datos bancarios recogidos -->
+        </ul>
+      </section>
+      <section class="bg-white shadow-md rounded-lg p-6 mb-8">
+        <h2 class="text-xl font-semibold mb-4">Credenciales</h2>
+        <ul>
+          <li><strong>Nombre de usuario:</strong> {{ 'hola' }}</li>
+          <li><strong>Contraseña:</strong> {{ 'hola' }}</li>
+          <!-- Agrega más detalles según las credenciales recogidas -->
+        </ul>
+      </section>
+      <section class="bg-white shadow-md rounded-lg p-6 mb-8">
+        <h2 class="text-xl font-semibold mb-4">Datos de contacto</h2>
+        <ul>
+          <li><strong>Email:</strong> {{ 'hola' }}</li>
+          <li><strong>Teléfono:</strong> {{ 'hola' }}</li>
+          <!-- Agrega más detalles según los datos de contacto recogidos -->
+        </ul>
+      </section>
+    </main>
   </MainLayout>
 </template>
